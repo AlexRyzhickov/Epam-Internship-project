058641755bb2e331fc1a1a8ee6cc43de96abb0e7
Alexander <alexandr-altukhov2013@yandex.ru>
2021-08-16 22:30:33 +0300

    fix: get shared preferences name from string resources

--- app/src/main/java/com/epam/bet/viewmodel/SharedPreferencesProvider.kt
+++ app/src/main/java/com/epam/bet/viewmodel/SharedPreferencesProvider.kt
@@ -4,13 +4,15 @@ import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.lifecycle.AndroidViewModel
+import com.epam.bet.R
+import com.epam.bet.MainApp
 
 class SharedPreferencesProvider(var application: Application){
     private lateinit var sharedPreferences: SharedPreferences
 
     fun get(key: String, defValue: String = "none"): String? {
         if (!this::sharedPreferences.isInitialized){
-            sharedPreferences = application.applicationContext.getSharedPreferences("BetAppSettings", Context.MODE_PRIVATE)
+            sharedPreferences = application.applicationContext.getSharedPreferences(MainApp.applicationContext().getString(R.string.app_name), Context.MODE_PRIVATE)
         }
         return sharedPreferences.getString(key, defValue)
     }
